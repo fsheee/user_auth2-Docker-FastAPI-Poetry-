a3686b6e652dee9433cfa8b6784776aa984adfa7
Afsheen Shafiq <fsheekhi@gmail.com>
2024-09-10 01:49:29 +0500

    Update db.py

--- user1_service/app/db.py
+++ user1_service/app/db.py
@@ -20,20 +20,3 @@ def get_session():
         yield session
 
 
-# from sqlmodel import create_engine, SQLModel, Session # type: ignore
-# from app import settings
-
-# # Create the connection string
-# connection_string: str = str(settings.DATABASE_URL).replace("postgresql", "postgresql+psycopg2")
-
-# # Create the database engine
-# engine = create_engine(connection_string, connect_args={}, pool_recycle=300)
-
-# # Create tables based on SQLModel metadata
-# def create_tables():
-#     SQLModel.metadata.create_all(engine)
-
-# # Create a session generator
-# def get_session():
-#     with Session(engine) as session:
-#         yield session
